d86efc9281478584bbd8f109e7805fbafb7ffaed
jaedeok95 <dlwoejr700@gmail.com>
2021-07-08 21:30:26 +0900

    베스트 앨범 시작

---
+++ Hash/베스트앨범/Solution.java
@@ -0,0 +1,40 @@
+package Hash.베스트앨범;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class Solution {
+    class Music {
+        String genre;
+        int number;
+        int play;
+
+        public Music(String genre, int number, int play){
+            this.genre = genre;
+            this.number = number;
+            this.play = play;
+        }
+    }
+     
+    public int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] answer = {};
+        String[] genres = {"classic","pop","classic","classic","pop"};
+        int[] plays = {500,600,150,800,2500};
+        
+
+
+        for(int i=0;i<genres.length;i++){
+            
+        }
+        
+        // 장르 내 많이 재생된 노래 먼저 수록
+        // 장르 내 재생횟수 같으면 고유 번호 낮은 노래 먼저 수록
+    }
+}
