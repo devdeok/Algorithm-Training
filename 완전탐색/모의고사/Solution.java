4da21b7c2b28d3333ac1f966a017eac997780de0
jaedeok95 <dlwoejr700@gmail.com>
2021-07-08 23:46:45 +0900

    모의고사

--- 완전탐색/모의고사/Solution.java
+++ 완전탐색/모의고사/Solution.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
-        int[] person1 = {1,2,3,4,5};
-        int[] person2 = {2,1,2,3,2,4,2,5};
-        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
-        int answer1=0, answer2 =0, answer3 =0;
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
+        int ans1=0, ans2 =0, ans3 =0;
         
         for(int i =0; i<answers.length; i++){
-            if(person1[i%person1.length] == answers[i]) answer1++;
-            if(person2[i%person2.length] == answers[i]) answer2++;
-            if(person3[i%person3.length] == answers[i]) answer3++;
+            if(p1[i%p1.length] == answers[i]) ans1++;
+            if(p2[i%p2.length] == answers[i]) ans2++;
+            if(p3[i%p3.length] == answers[i]) ans3++;
         }
-        int max = Math.max(Math.max(answer1, answer2),answer3);
+        int max = Math.max(Math.max(ans1, ans2),ans3);
         ArrayList<Integer> list = new ArrayList<Integer>();
-        if(max==answer1) list.add(1);
-        if(max==answer2) list.add(2);
-        if(max==answer3) list.add(3);
+        if(max==ans1) list.add(1);
+        if(max==ans2) list.add(2);
+        if(max==ans3) list.add(3);
         
         answer = new int[list.size()];
         
