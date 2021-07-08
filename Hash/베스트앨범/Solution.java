caee4841722a4ca54adc64329ad9ade77aa91d4f
jaedeok95 <dlwoejr700@gmail.com>
2021-07-08 22:07:43 +0900

    모의고사

--- Hash/베스트앨범/Solution.java
+++ Hash/베스트앨범/Solution.java
@@ -1,22 +1,11 @@
 package Hash.베스트앨범;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class Solution {
-    class Music {
-        String genre;
-        int number;
-        int play;
-
-        public Music(String genre, int number, int play){
-            this.genre = genre;
-            this.number = number;
-            this.play = play;
-        }
-    }
      
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
@@ -27,12 +16,18 @@ public class Solution {
         int[] answer = {};
         String[] genres = {"classic","pop","classic","classic","pop"};
         int[] plays = {500,600,150,800,2500};
-        
 
-
-        for(int i=0;i<genres.length;i++){
-            
+        ArrayList<Integer> arrAnswer = new ArrayList<Integer>();
+        HashMap<String, Integer> hmGenres = new HashMap<>();
+          for(int i =0; i< genres.length; i++){
+             hmGenres.put(genres[i], hmGenres.getOrDefault(genres[i],0)+plays[i]);
         }
+         
+        List<String> keySetList = new ArrayList<>(hmGenres.keySet());
+        Collections.sort(keySetList, (o1, o2) -> (hmGenres.get(o2).compareTo(hmGenres.get(o1)))); // 내림차순으로 정렬
+        
+        System.out.println(keySetList.get(0));
+        System.out.println(keySetList.get(1));
         
         // 장르 내 많이 재생된 노래 먼저 수록
         // 장르 내 재생횟수 같으면 고유 번호 낮은 노래 먼저 수록
---
+++ 완전탐색/모의고사/Solution.java
@@ -0,0 +1,14 @@
+package 완전탐색.모의고사;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
