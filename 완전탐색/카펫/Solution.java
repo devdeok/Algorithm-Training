ec4c47a190fe52d7dcab395fdda0b0a8418da4d2
jaedeok95 <dlwoejr700@gmail.com>
2021-07-11 00:14:14 +0900

    카펫

---
+++ 완전탐색/카펫/Solution.java
@@ -0,0 +1,31 @@
+package 완전탐색.카펫;
+
+public class Solution{
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        int n;
+
+        answer[0] = 3;
+        answer[1] = 3;
+
+        for(int i=3; i<=sum;i++){
+            if(sum%i==0){
+                n=sum/i;
+                if((i-2)*(n-2)==yellow && n>i) return new int[]{n,i};
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int brown = 24;
+        int yellow = 24;
+        int[] ans = s.solution(brown, yellow);
+        
+        for(int arg : ans){
+            System.out.println(arg);
+        }
+    }
+}
