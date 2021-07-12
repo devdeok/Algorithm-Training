6993b85ac3ea12762216095326883849798eb0c7
jaedeok <dlwoejr700@gmail.com>
2021-07-13 04:27:45 +0900

    H-Index

--- 정렬/H_Index/Solution.java
+++ 정렬/H_Index/Solution.java
@@ -1,11 +1,24 @@
 package 정렬.H_Index;
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
+        // int[] arr = Arrays.stream(citations).
+        //             boxed().sorted(Collections.reverseOrder()).
+        //             mapToInt(Integer::intValue).toArray(); <-- 내림차순
+
+        Arrays.sort(citations);
+
+        // 피인용수가 논문수보다 작아지기 시작하는 수가 h-index
+
+        int max = 0;
+        for(int i=citations.length-1; i>-1; i--){
+            int min = Math.min(citations[i],citations.length-i);
+            if(max < min) max = min;
+        }
         
-        
-        return answer;
+        return max;
     }
 
     public static void main(String[] args) {
