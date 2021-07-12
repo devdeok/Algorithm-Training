8394255feb84b74ea05fd5e0201a841867952d7b
jaedeok <dlwoejr700@gmail.com>
2021-07-13 02:00:27 +0900

    H-Idex

---
+++ 정렬/H_Index/Solution.java
@@ -0,0 +1,18 @@
+package 정렬.H_Index;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = {3,0,6,1,5};
+        Solution s = new Solution();
+
+        System.out.println(s.solution(citations));
+
+    }
+}
