6f409705ad6dc008fc514eede8b58c18c3cc1e12
jaedeok <dlwoejr700@gmail.com>
2022-02-06 04:33:55 +0900

    실패율

--- Programmers/level_1/실패율/Solution.py
+++ Programmers/level_1/실패율/Solution.py
@@ -2,30 +2,23 @@ import operator
 # 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수/스테이지에 도달한 플레이어 수
 # N : 스테이지 개수, stages : 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열
 def solution(N, stages) :
-    count = [0] * N
-    dic =  {}
+    dic={} # 실패확률
+    total_num = len(stages) #총인원
 
-    for i in range(N) :
-        num = 0
-        for j in stages :
-            if j>=i+1 :
-                count[i]+=1
-            if j==i+1 :
-                num+=1
-        dic[i+1] = num/count[i]
+    for i in range(1,N+1) : 
+        if total_num != 0 :
+            dic[i] = stages.count(i)/total_num # 남아있는 인원수/총 인원수
+            total_num -= stages.count(i) # 총 인원수 - 앞에 남아있는 인원수
+        else :
+            dic[i] = 0
     
-    # 0은 Key, 1은 Value
-    sorted_dic = dict(sorted(dic.items(), key=operator.itemgetter(1), reverse=True))
-    ans = list(sorted_dic)
-    
-    return ans # 실패율이 높은 스테이지부터 내림차순으로 정렬
+    # 0이면 key기준, 1이면 value기준
+    return list(dict(sorted(dic.items(), key=operator.itemgetter(1), reverse=True))) # 실패율이 높은 스테이지부터 내림차순으로 정렬
 
-def main() :
-    N = 5
-    stages = [2,1,2,6,2,4,3,3] # 3,4,2,1,5
-    # stages = [4,4,4,4,4]
+# N = 5
+# stages = [2,1,2,6,2,4,3,3]
 
-    print(solution(N,stages))
+N = 4
+stages = [4,4,4,4,4]
 
-if __name__ == '__main__' :
-    main()
+print(solution(N,stages))
